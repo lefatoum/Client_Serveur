9a06bc334dd8b3a35530ab831c37c7f8e78d66ff
Chris <lefatoum@hotmail.fr>
2020-09-18 18:26:48 +0200

    Modication multiChatClient

--- multiChatClient.py
+++ multiChatClient.py
@@ -1,4 +1,4 @@
-""" Script for Tkinter GUI chat client. """
+
 
 import tkinter
 from socket import AF_INET, socket, SOCK_STREAM
@@ -6,18 +6,17 @@ from threading import Thread
 
 
 def receive():
-    """ Handles receiving of messages. """
+
     while True:
         try:
             msg = sock.recv(BUFSIZ).decode("utf8")
             msg_list.insert(tkinter.END, msg)
-        except OSError:  # Possibly client has left the chat.
-            break
+        except OSError:
 
 def send(event=None):
     """ Handles sending of messages. """
     msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
+    my_msg.set("")
     sock.send(bytes(msg, "utf8"))
     if msg == "#quit":
         sock.close()
@@ -30,12 +29,12 @@ def on_closing(event=None):
 
 def smiley_button_tieup(event=None):
     """ Function for smiley button action """
-    my_msg.set(":)")    # A common smiley character
+    my_msg.set(":)")
     send()
 
 def sad_button_tieup(event=None):
     """ Function for smiley button action """
-    my_msg.set(":(")    # A common smiley character
+    my_msg.set(":(")
     send()
 
 
@@ -43,9 +42,9 @@ top = tkinter.Tk()
 top.title("Simple Chat Client v1.0")
 messages_frame = tkinter.Frame(top)
 
-my_msg = tkinter.StringVar()  # For the messages to be sent.
+my_msg = tkinter.StringVar()
 my_msg.set("")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+scrollbar = tkinter.Scrollbar(messages_frame)
 msg_list = tkinter.Listbox(messages_frame, height=15, width=70, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
@@ -81,4 +80,4 @@ sock.connect(ADDR)
 
 receive_thread = Thread(target=receive)
 receive_thread.start()
-tkinter.mainloop()  # Starts GUI execution.
+tkinter.mainloop()  
